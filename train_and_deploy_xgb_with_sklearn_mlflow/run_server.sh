4acbb8dfc1829477b9bb402c73eb566d38f4bcb6
eliran <eboraks@gmail.com>
2023-03-05 16:31:09 -0500

    Setting mlflow server run script to use default-artifact-root

--- train_and_deploy_xgb_with_sklearn_mlflow/run_server.sh
+++ train_and_deploy_xgb_with_sklearn_mlflow/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mlflow server --backend-store-uri sqlite:///mlruns.db --serve-artifacts --artifacts-destination file:mlartifacts --host 0.0.0.0
+mlflow server --backend-store-uri sqlite:///mlruns.db --serve-artifacts --default-artifact-root file:mlartifacts --host 0.0.0.0
